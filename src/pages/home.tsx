06b6db8cddd53789feed0b9cd489aa2c20fbcc96
autonomoose <todd@werner-digital.com>
2022-01-18 20:29:03 -0500

    (refactor)use schedgroup comp

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,6 +10,7 @@ import {OptionsButtons, buildButtons, buildOptions} from '../components/schedbut
 import PageTopper from '../components/pagetopper';
 import Seo from '../components/seo';
 import { fetchEventsDB } from '../components/eventsutil';
+import { fetchSchedGroupsDB } from '../components/schedgrputil';
 
 import { useSnackbar } from 'notistack';
 import Backdrop from '@mui/material/Backdrop';
@@ -435,83 +436,23 @@ const HomePage = () => {
       fetchData();
     }, [enqueueSnackbar] );
 
+    // load all schedules, groups
     useEffect(() => {
       const fetchData = async () => {
           try {
-              const result: any = await API.graphql({query: listSchedGroupsFull})
-              console.log("schedgroups records:", result.data.listSchedGroups.items.length);
-
-              const compactEvents = result.data.listSchedGroups.items.reduce((resdict: iGrpSchedTask, item: iSchedGroupListDB) => {
-                  const evkeys = item.evnames.split('!');
-                  // group,sched,ev
-                  let wkGroup = evkeys[0];
-                  let wkSched = evkeys[1];
-
-                  if (wkSched !== 'args' && evkeys[2] && evkeys[2] !== 'args') {
-                      if (!resdict[wkGroup+"!"+wkSched]) {
-                          resdict[wkGroup+"!"+wkSched] = [];
-                      }
-                      resdict[wkGroup+"!"+wkSched].push({evTaskId: evkeys[2]});
-                  }
-                  return resdict;
-              }, {});
-
-              // console.log("eventlist", compactEvents);
-              const compactGroups = result.data.listSchedGroups.items.reduce((resdict: iSchedGroupList, item: iSchedGroupListDB) => {
-                  const evkeys = item.evnames.split('!');
-                  // group,sched,ev
-                  let wkGroup = evkeys[0];
-                  if (!resdict[wkGroup]) {
-                      // setup basic group
-                      resdict[wkGroup] = {descr: '', schedNames: []};
-                  }
-                  if (evkeys[1] === 'args') {
-                      // group args
-                      resdict[wkGroup].descr = (item.descr)? item.descr: '';
-                  } else if (evkeys[2] === 'args') {
-                      // schedule args
-                      const wkSched = evkeys[1];
-                      let schedArgs: iSchedule = {schedName: wkSched, schedTasks: []};
-                      let wkSound: iEvsSound = {};
-
-                      if (compactEvents[wkGroup+"!"+wkSched]) {
-                          schedArgs.schedTasks = compactEvents[wkGroup+"!"+wkSched];
-                      } else {
-                          schedArgs.schedTasks = [];
-                      }
-                      if (item.begins) {
-                          schedArgs.begins = item.begins;
-                      }
-                      if (item.button) {
-                          schedArgs.buttonName = item.button;
-                      }
-                      if (item.sound) {
-                          wkSound['name'] = item.sound;
-                          console.log('sound', item.sound);
-                      }
-                      if (item.soundrepeat) {
-                          wkSound['repeat'] = parseInt(item.soundrepeat, 10);
-                          console.log('soundrepeat', item.soundrepeat);
-                      }
-                      if (item.warn || item.warn === '') {
-                          console.log('warn', item.warn);
-                          schedArgs['warn'] = {};
-                      }
-                      if (wkSound && Object.keys(wkSound).length > 0) {
-                          schedArgs['sound'] = wkSound;
-                      }
-                      resdict[wkGroup].schedNames.push(schedArgs)
-                  }
-                  return resdict;
-              }, {});
-
-              enqueueSnackbar(`loaded schedules`,
-                {variant: 'info', anchorOrigin: {vertical: 'bottom', horizontal: 'right'}} );
-              setSchedGroups(compactGroups);
+              const newSchedgrps = await fetchSchedGroupsDB();
+              if (newSchedgrps) {
+                  enqueueSnackbar(`loaded schedules`,
+                    {variant: 'info', anchorOrigin: {vertical: 'bottom', horizontal: 'right'}} );
+                  setSchedGroups(newSchedgrps);
+              } else {
+                  enqueueSnackbar(`no schedules found`, {variant: 'error'});
+              }
           } catch (result) {
-              enqueueSnackbar(`error retrieving schedule groups`, {variant: 'error'});
+              enqueueSnackbar(`error retrieving sched/groups`, {variant: 'error'});
               console.log("got error", result);
           }
+
       };
 
       fetchData();
