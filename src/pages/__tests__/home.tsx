a6beba63a70bcc1eaea3b604d79aa18ee9514c37
autonomoose <todd@werner-digital.com>
2022-04-02 19:44:22 -0400

    (tests) buttons test for home

--- src/pages/__tests__/home.tsx
+++ src/pages/__tests__/home.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { render, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 
 import HomePage from "../home";
 
 // needs mocks for DisplayFutureEvent, {DisplayFutureCard, buildFutureEvents} from '../components/futurevents';
-jest.mock('../../components/schedbuttons', () => ({
-    ...jest.requireActual('../../components/schedbuttons'),
-    buildButtons: jest.fn(() => Promise.resolve({
-      testsched: 'test1',
-      })),
-    buildOptions: jest.fn(() => Promise.resolve({
-      tomorrow: false,
-      })),
-}));
+jest.unmock('../../components/schedbuttons');
 
 jest.mock('../../components/eventsutil', () => ({
     fetchEventsDB: jest.fn(() => Promise.resolve(
@@ -25,15 +17,23 @@ jest.mock('../../components/eventsutil', () => ({
 
 jest.mock('../../components/schedgrputil', () => ({
     ...jest.requireActual('../../components/schedgrputil'),
-    fetchSchedGroupsDB: jest.fn(() => Promise.resolve(
-      {default: {descr: 'test group',
+    fetchSchedGroupsDB: jest.fn(() => Promise.resolve( {
+      default: {descr: 'test group',
         schedNames: [{
           begins: 'now',
           buttonName: 'test1',
           descr: 'test sched',
           schedName: 'testsched',
           schedTasks: [{evTaskId: 'testev'}],
-          }]}
+      }]},
+      test2: {descr: 'test2 group',
+        schedNames: [{
+          begins: 'now',
+          buttonName: 'test2',
+          descr: 'test2 sched',
+          schedName: 'testsched',
+          schedTasks: [{evTaskId: 'testev'}],
+      }]},
       })),
 }));
 
@@ -55,9 +55,14 @@ const mySetup = async () => {
     await waitFor(() => {
         expect(utils.getByTestId('dataBackdrop')).not.toBeVisible();
     });
-
+    const groupInput = utils.getByTestId(/schedgroup/i);
+    const groupInputChg = (myvalue: string) => {
+        fireEvent.change(groupInput, { target: { value: myvalue } });
+    };
     return {
         ...utils,
+        groupInput,
+        groupInputChg,
     }
 }
 
@@ -87,6 +92,50 @@ describe("HomePage", () => {
       await waitFor(() => {
         expect(utils.getByTestId('clock-scheduler')).toBeVisible();
       });
+  });
+  it("starts and stops scheduler", async () => {
+    const utils = await mySetup();
+    userEvent.click(utils.getByRole('button', {name: /test1/i}));
+
+    // handles multiple presses
+    userEvent.click(utils.getByRole('button', {name: /test1/i}));
+
+    userEvent.click(utils.getByRole('button', {name: /off/i}));
+    expect(mockEnqueue).toHaveBeenLastCalledWith(`scheduler off`, {variant: 'info', "anchorOrigin": {"horizontal": "right", "vertical": "bottom"},});
+  });
+
+  it("changes group to test2", async () => {
+    const utils = await mySetup();
+    expect(utils.groupInput).toHaveValue('default');
+
+    utils.groupInputChg('test2');
+    await waitFor(() => {
+      expect(utils.groupInput).toHaveValue('test2');
+    });
+  });
+
+  it("stops scheduler with group change", async () => {
+    const utils = await mySetup();
+    userEvent.click(utils.getByRole('button', {name: /test1/i}));
+
+    utils.groupInputChg('test2');
+    await waitFor(() => {
+      expect(utils.groupInput).toHaveValue('test2');
+    });
+    expect(mockEnqueue).toHaveBeenLastCalledWith(`scheduler canceled`, {variant: 'info', "anchorOrigin": {"horizontal": "right", "vertical": "bottom"},});
 
   });
+
+  it("changes options", async () => {
+    const utils = await mySetup();
+    userEvent.click(utils.getByRole('button', {name: /tomorrow/i}));
+  });
+
+  it("restarts active schedule when changes options", async () => {
+    const utils = await mySetup();
+    userEvent.click(utils.getByRole('button', {name: /test1/i}));
+
+    userEvent.click(utils.getByRole('button', {name: /tomorrow/i}));
+  });
+
 });
