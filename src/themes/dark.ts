334c10844aaf370bf0fd79a50f8e16ef98f153c6
autonomoose <todd@werner-digital.com>
2022-06-06 13:00:22 -0400

    (refactor) and tweek colors

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -3,5 +3,9 @@ import { ThemeOptions } from "@mui/material";
 export const darkTheme: ThemeOptions = {
   palette: {
     mode: "dark",
+    background: {
+      paper: "#121212",
+      default: "#090909",
+    },
   },
 };
